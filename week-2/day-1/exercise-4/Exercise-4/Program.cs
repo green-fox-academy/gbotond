446aa1c8397e3ee7382e48bbf2e7a1fe514217dd
GBotond <botond.gorfol@gmail.com>
2017-09-11 14:33:39 +0200

    Exercise-4

---
+++ week-2/day-1/exercise-4/Exercise-4/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Exercise_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Botond");
+            Console.WriteLine("28");
+            Console.WriteLine("1.80");
+            Console.ReadLine();
+        }
+    }
+}
