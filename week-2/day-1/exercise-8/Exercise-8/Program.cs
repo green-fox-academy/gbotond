c127b56165e6833fd0a69e938c078a32bcf0b8e5
GBotond <botond.gorfol@gmail.com>
2017-09-11 16:13:06 +0200

    Exercise-8

---
+++ week-2/day-1/exercise-8/Exercise-8/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Exercise_8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int a = 4, b = 6;
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            a = a ^ b;
+            b = a ^ b;
+            a = a ^ b;
+            Console.WriteLine("After Swapping");
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+
+            Console.ReadLine();
+        }
+    }
+}
