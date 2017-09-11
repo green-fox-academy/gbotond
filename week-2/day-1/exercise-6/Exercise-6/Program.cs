32de07eab2d68bd13445210abd2d6e648bf2858b
GBotond <botond.gorfol@gmail.com>
2017-09-11 15:32:41 +0200

    Exercise-6

---
+++ week-2/day-1/exercise-6/Exercise-6/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Exercise_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Weekly hours:");
+            Console.WriteLine(5 * 6);
+            Console.Write("Semester:");
+            Console.WriteLine(17 * 30);
+            Console.Write("Percentage:");
+            Console.WriteLine("58");
+
+            Console.ReadLine();
+        }
+    }
+}
