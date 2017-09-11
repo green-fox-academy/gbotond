f2641d71df743b2b2b6c32d5f750e429880c6479
GBotond <botond.gorfol@gmail.com>
2017-09-11 16:39:02 +0200

    Exercise-9

---
+++ week-2/day-1/exercise-9/Exercise-9/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Exercise_9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string yourname = "Botond";
+            Console.WriteLine(yourname);
+
+            int intValue1 = 28;
+            Console.WriteLine(intValue1);
+
+            double a = 1.8;
+            Console.WriteLine(a);
+
+            bool married = false;
+            Console.WriteLine(married);
+
+
+            Console.ReadLine();
+        }
+    }
+}
