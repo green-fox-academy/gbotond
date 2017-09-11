01b7678616b5cc9b0da60e31eb3c0ace39ddfaeb
GBotond <botond.gorfol@gmail.com>
2017-09-11 15:44:18 +0200

    Exercise-7

---
+++ week-2/day-1/exercise-7/Exercise-7/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Exercise_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 27;
+            Console.Write("My favorite number is: ");
+            Console.WriteLine(a);
+
+            Console.ReadLine();
+        }
+    }
+}
