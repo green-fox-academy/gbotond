9cea18ff472b8bebcb0f5914a2ae65dda27c322b
GBotond <botond.gorfol@gmail.com>
2017-09-11 17:28:44 +0200

    Exercise-18

---
+++ week-2/day-1/exercise-18/Exercise-18/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Exercise_18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i;
+            Console.WriteLine("enter a number");
+            while (int.TryParse(Console.ReadLine(), out i))
+            {
+                Console.WriteLine(i % 2 == 0 ? "even" : "odd");
+                Console.WriteLine("enter again");
+            }
+        }
+    }
+}
