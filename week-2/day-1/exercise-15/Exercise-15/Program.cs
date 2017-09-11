a1c2e6c30ad4289b7e3873ac16e0930c61cc1772
GBotond <botond.gorfol@gmail.com>
2017-09-11 17:13:59 +0200

    Exercise-15

---
+++ week-2/day-1/exercise-15/Exercise-15/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Exercise_15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("How far is that?");
+
+            int a = 100;
+            Console.WriteLine(a + "Kilometers");
+
+            double b = 100;
+            b *= 1.6;
+            Console.WriteLine(b + "Miles");
+
+            Console.ReadLine();
+        }
+    }
+}
