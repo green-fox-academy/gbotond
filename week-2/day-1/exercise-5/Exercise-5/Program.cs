a5a1b89c65bd7931c717932c66bc3b49e572a2d1
GBotond <botond.gorfol@gmail.com>
2017-09-11 14:51:57 +0200

    Exercise-5

---
+++ week-2/day-1/exercise-5/Exercise-5/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Exercise_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(13 + 22);
+            Console.WriteLine(22 - 13);
+            Console.WriteLine(22 * 13);
+            Console.WriteLine(22 / 13);
+            Console.WriteLine(22 % 13);
+            Console.WriteLine(22 % 13);
+
+            Console.ReadLine();
+        }
+    }
+}
