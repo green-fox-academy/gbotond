c062d311c0b1c747c86f31ffe2f42b10990cf86c
GBotond <botond.gorfol@gmail.com>
2017-09-11 14:14:29 +0200

    Exercise-3

---
+++ week-2/day-1/exercise-3/Exercise-3/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Exercise_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, Csilla!");
+            Console.WriteLine("Hello, Kriszta!");
+            Console.WriteLine("Hello, Tomi!");
+            Console.ReadLine();
+        }
+    }
+}
