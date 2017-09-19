9c657f174952f074c66d6ba1e05c279346adbd97
GBotond <botond.gorfol@gmail.com>
2017-09-19 13:55:04 +0200

    writesingleline

---
+++ week-3/day-2/WriteSingleLine/WriteSingleLine/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+
+namespace WriteSingleLine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Botond";
+            string path = @"C:\Users\Botond\Desktop\wsl.txt";
+            try
+            {
+                using (StreamWriter writer = new StreamWriter(path))
+                {
+                    writer.WriteLine(name);
+                }
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Unable to write file: wsl.txt");
+            }
+        }
+    }
+}
