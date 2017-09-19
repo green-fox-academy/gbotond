571f743564418eef8ab0d00fd0c2969c28652e38
GBotond <botond.gorfol@gmail.com>
2017-09-19 13:14:54 +0200

    CountLines

---
+++ week-3/day-2/CountLines/CountLines/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+
+namespace CountLines
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CountLines();
+            Console.ReadLine();
+        }
+        private static void CountLines()
+        {
+            try
+            {
+                string name = @"C:\Users\Botond\Desktop\cls.txt";
+                int lines = File.ReadAllLines(name).Length;
+                Console.WriteLine(lines);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("0");
+            }                       
+        }
+    }
+}
