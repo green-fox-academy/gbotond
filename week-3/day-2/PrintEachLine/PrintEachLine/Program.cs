08f614ed7203d7625e7b6cf96b01a9406616d33b
GBotond <botond.gorfol@gmail.com>
2017-09-19 11:43:51 +0200

    printeachline

---
+++ week-3/day-2/PrintEachLine/PrintEachLine/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+
+namespace PrintEachLine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = "my-file.txt";
+            try
+            {
+                String[] content = File.ReadAllLines(path);
+                Console.WriteLine(content);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Unable to read file: my-file.txt");
+            }
+            Console.ReadLine();
+        }
+    }
+}
