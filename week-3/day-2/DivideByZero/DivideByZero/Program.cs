b39cdfd689331ad2c57b3eab802ad83adec987a0
GBotond <botond.gorfol@gmail.com>
2017-09-19 11:41:15 +0200

    dividebyzero

---
+++ week-3/day-2/DivideByZero/DivideByZero/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace DivideByZero
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Divide();
+            Console.ReadLine();
+        }
+        private static void Divide()
+        {
+            try
+            {
+                int divisor = int.Parse(Console.ReadLine());
+                int result = 10 / divisor;
+                Console.WriteLine(result);
+            }
+            catch (DivideByZeroException error)
+            {
+                Console.WriteLine("Can't divide by zero!");
+            }
+            catch (Exception error)
+            {
+                Console.WriteLine("Damn! Something wrong happened!");
+            }
+            finally
+            {
+                Console.ReadLine();
+            }
+        }
+    }
+}
