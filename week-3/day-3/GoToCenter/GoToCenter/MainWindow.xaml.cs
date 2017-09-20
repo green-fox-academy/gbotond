31e08df981bd8a5cebf4019334666e43fc8921fa
GBotond <botond.gorfol@gmail.com>
2017-09-20 13:22:46 +0200

    GoToCenter complete

---
+++ week-3/day-3/GoToCenter/GoToCenter/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using GreenFox;
+
+namespace GoToCenter
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            var foxDraw = new FoxDraw(canvas);
+
+            DrawLine(75, 50);
+            DrawLine(150, 50);
+            DrawLine(225, 50);
+
+            void DrawLine(int x, int y)
+            {
+                foxDraw.StrokeColor(Colors.Blue);
+                foxDraw.DrawLine(new Point(x, y), new Point(canvas.Width / 2, canvas.Height / 2));
+            }
+        }
+    }
+}
