c207a7245f7ce12985d74d071378d8929f170288
GBotond <botond.gorfol@gmail.com>
2017-09-20 16:16:17 +0200

    RainBowFunction complete

---
+++ week-3/day-3/RainBowFunction/RainBowFunction/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using GreenFox;
+
+namespace RainBowFunction
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            var foxDraw = new FoxDraw(canvas);
+
+            Random r = new Random();
+
+            for (int size = 150; size > 0; size -= 10)
+            {
+                RainBowFunction(size, (Color.FromArgb((byte)r.Next(256), (byte)r.Next(256), (byte)r.Next(256), (byte)r.Next(256))), foxDraw);               
+            }
+        }       
+
+        public void RainBowFunction(int size, Color randomColor, FoxDraw foxDraw)
+        {
+            foxDraw.FillColor(randomColor);
+            foxDraw.DrawRectangle((canvas.Width - size)/2, (canvas.Height - size)/2, size, size);
+        }
+    }
+}
