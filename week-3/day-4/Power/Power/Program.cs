74d5574c062b2ad0d1af3d6d86ac17f422451e54
GBotond <botond.gorfol@gmail.com>
2017-09-21 12:51:37 +0200

    Power complete

---
+++ week-3/day-4/Power/Power/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Power
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int expInt = 5;
+            int baseInt = 3;
+
+            Console.WriteLine(Power(baseInt, expInt));
+            Console.ReadLine();
+        }
+        public static int Power(int baseInt, int expInt)
+        {
+            if (expInt == 0)
+            {
+                return 1;
+            }
+            else
+                return baseInt * Power(baseInt, expInt - 1);        
+        }
+    }
+}
