bf77205a60c5ffd1ee3b76b58bc61ca0876114cd
GBotond <botond.gorfol@gmail.com>
2017-09-21 16:58:12 +0200

    StringsAgain complete

---
+++ week-3/day-4/StringsAgain/StringsAgain/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringsAgain
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "yxcyxcyxcyxc";
+            Console.WriteLine(CharRemove(str));
+            Console.ReadLine();
+        }
+        public static string CharRemove(string str)
+        {
+            if (str.Length < 1)
+            {
+                return str;
+            }
+            else if (str[0] == 'x')
+            {
+                return CharRemove(str.Substring(1));
+            }
+            else
+                return str[0] + CharRemove(str.Substring(1));
+        }
+    }
+}
