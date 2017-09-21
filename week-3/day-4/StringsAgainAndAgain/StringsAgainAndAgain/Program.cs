bdd44c5400cd50e3e342b1edd5de458d1a2eb72b
GBotond <botond.gorfol@gmail.com>
2017-09-21 17:19:26 +0200

    StringsAgainAndAgain complete

---
+++ week-3/day-4/StringsAgainAndAgain/StringsAgainAndAgain/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringsAgainAndAgain
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "greenfox";
+            Console.WriteLine(CharAdd(str));
+            Console.ReadLine();
+        }
+        public static string CharAdd(string str)
+        {
+            if (str.Length < 1)
+            {
+                return str;
+            }
+            return str[0] + "*" + CharAdd(str.Substring(1));
+        }
+    }
+}
