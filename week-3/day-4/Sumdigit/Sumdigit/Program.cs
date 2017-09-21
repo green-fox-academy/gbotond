67e052a2bb7b67f9704898ee5e9cd7ac019817c5
GBotond <botond.gorfol@gmail.com>
2017-09-21 11:46:42 +0200

    Sumdigit complete

---
+++ week-3/day-4/Sumdigit/Sumdigit/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sumdigit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(sumDigitsRecursion(123, 0));
+            Console.ReadLine();
+        }
+
+        public static long sumDigitsRecursion(int n, long sum)
+        {
+            if (n == 0)
+            {
+                return sum;
+            }
+            else
+            {
+                sum = sum + n % 10;
+                n = n / 10;
+                return sumDigitsRecursion(n, sum);
+            }
+        }
+    }
+}
