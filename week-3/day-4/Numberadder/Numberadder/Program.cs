029cbb09ca4f9122c878ae1530e7209ce1a6b950
GBotond <botond.gorfol@gmail.com>
2017-09-21 11:23:28 +0200

    Numberadder complete

---
+++ week-3/day-4/Numberadder/Numberadder/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Numberadder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 1;
+            Recursion(27, n);
+            Console.ReadLine();
+        }
+        public static int Recursion(int input, int n)
+        {
+            Console.WriteLine(n);
+            if (n == input)
+            {
+                return 1;
+            }
+            else
+            {
+                return Recursion(input, n + 1);
+            }
+        }
+    }
+}
