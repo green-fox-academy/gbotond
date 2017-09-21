d7d8fc60bfdeda9a3e1e1d31ba7179822b0a3fa0
GBotond <botond.gorfol@gmail.com>
2017-09-21 10:03:55 +0200

    PurpleSteps3d

---
+++ week-3/day-3/PurpleSteps3d/PurpleSteps3d/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using GreenFox;
+
+namespace PurpleSteps3d
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            var foxDraw = new FoxDraw(canvas);
+
+            double x = 10;
+            double y = 10;
+            double size = 10;
+
+            for (int i = 0; i < 6; i++)
+            {
+                foxDraw.StrokeColor(Color.FromRgb(102, 92, 111));
+                foxDraw.FillColor(Color.FromRgb(177, 70, 244));
+                foxDraw.DrawRectangle(x, y, size, size);
+                x += size;
+                y += size;
+                size += 10;
+            }
+        }  
+    }
+}
