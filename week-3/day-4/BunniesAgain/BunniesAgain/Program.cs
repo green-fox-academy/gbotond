b0aad6755a1234d51c6ba326c2f2f465ead1d57b
GBotond <botond.gorfol@gmail.com>
2017-09-21 16:08:33 +0200

    BunniesAgain complete

---
+++ week-3/day-4/BunniesAgain/BunniesAgain/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BunniesAgain
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(EarCounter(8));
+            Console.ReadLine();
+        } 
+        public static int EarCounter(int n)
+        {
+            if ((n == 0))
+            {
+                return 0;
+            } 
+            else if (n % 2 == 0)
+            {
+                return 3 + EarCounter(n - 1);
+            }
+            else
+            {
+                return 2 + EarCounter(n - 1);
+            }
+        }
+    }
+}
