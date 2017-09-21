86cdea2b0aa31dd57e28188246609a26ed271c11
GBotond <botond.gorfol@gmail.com>
2017-09-21 11:11:43 +0200

    Counter complete

---
+++ week-3/day-4/Counter/Counter/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Counter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FactorialRecursion(5);
+            Console.ReadLine();
+        }
+
+        public static int FactorialRecursion(int n)
+        {
+            Console.WriteLine(n);
+            if (n == 1)
+            {
+                return 1;
+            }
+            else
+            {
+                return n * FactorialRecursion(n - 1);
+            }
+        }
+    }
+}
