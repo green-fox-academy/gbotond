2243451c5361a9ec67447acc8e7b8ff5cec2d4ed
GBotond <botond.gorfol@gmail.com>
2017-09-21 16:41:11 +0200

    Strings complete

---
+++ week-3/day-4/Strings/Strings/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "xyxyxyxy";
+            Console.WriteLine(CharChange(str));
+            Console.ReadLine();
+        }
+        public static string CharChange(string str)
+        {
+            if (str.Length < 1)
+            {
+                return str;
+            }
+            else if (str[0] == 'x')
+            {
+                return 'y' + CharChange(str.Substring(1));
+            }
+            else
+                return str[0] + CharChange(str.Substring(1));
+        }
+    }
+}
