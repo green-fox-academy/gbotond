e1277b8a62abdb1b67dc230b6357a2b6b96f7062
GBotond <botond.gorfol@gmail.com>
2017-09-21 13:04:57 +0200

    Bunnies complete

---
+++ week-3/day-4/Bunnies/Bunnies/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bunnies
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 8;
+            int m = 2;
+
+            Console.WriteLine(Ears(n, m));
+            Console.ReadLine();
+        }
+        public static int Ears(int n , int m)
+        {
+            if (m == 1)
+            {
+                return n;
+            }
+            else
+                return n + Ears(n, m - 1);
+        }
+    }
+}
