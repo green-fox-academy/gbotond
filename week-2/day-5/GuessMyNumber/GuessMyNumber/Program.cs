86dab4c5ff3de40fbc4dd6e6818e2276aa1c28bb
GBotond <botond.gorfol@gmail.com>
2017-09-24 18:44:58 +0200

    GuessMyNumber complete

---
+++ week-2/day-5/GuessMyNumber/GuessMyNumber/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace GuessMyNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random n = new Random();
+
+            int val = n.Next(1, 100);
+            int guess = 0;
+            bool correct = false;
+
+            Console.WriteLine("I'm thinking of a number between 1 and 100.");
+
+            while (!correct)
+            {
+                Console.Write("Guess: ");
+                string input = Console.ReadLine();
+
+                if (!int.TryParse(input, out guess))
+                {
+                    Console.WriteLine("That's not a number.");
+                    continue;
+                }
+                if (guess < val)
+                {
+                    Console.WriteLine("No, the number I'm thinking is higher than that number.");
+                }
+                else if (guess > val)
+                {
+                    Console.WriteLine("No, the number I'm thinking is lower than that number.");
+                }
+                else
+                {
+                    correct = true;
+                    Console.WriteLine("You guessed right!");
+                    Console.ReadLine();
+                }
+            }
+        }
+    }
+}
