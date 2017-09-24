779ea45e99b0df383483f218738145687d7e697f
GBotond <botond.gorfol@gmail.com>
2017-09-24 18:49:18 +0200

    DrawPyramid complete

---
+++ week-2/day-1/DrawPyramid/DrawPyramid/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace DrawPyramid
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Write a program that reads a number from the standard input, then draws a
+            // pyramid like this:
+            //
+            //
+            //    *
+            //   ***
+            //  *****
+            // *******
+            //
+            // The pyramid should have as many lines as the number was
+            int pyramid = 4;
+            for (int i = 1; i <= pyramid; i++)
+            {
+                for (int j = i; j <= pyramid -1; j++)
+                {
+                    Console.Write(" ");
+                }
+                for (int k = 1; k <= 2 * i - 1; k++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }  
+            Console.ReadLine();
+        }
+    }
+}
