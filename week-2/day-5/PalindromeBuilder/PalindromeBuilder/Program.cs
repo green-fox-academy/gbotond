51515dbd61e2ece63769aa144b8472e94428b5e0
GBotond <botond.gorfol@gmail.com>
2017-09-24 18:45:49 +0200

    PalindromeBuilder complete

---
+++ week-2/day-5/PalindromeBuilder/PalindromeBuilder/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace PalindromeBuilder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pal = "Greenfox";
+            CreatePalindrome(pal);
+            Console.Write(pal);
+            Console.WriteLine(CreatePalindrome(pal));
+            Console.ReadLine();
+        }
+
+        public static string CreatePalindrome(string pal)
+        {
+            char[] chars = pal.ToCharArray();
+            int j = chars.Length - 1;
+            for (int i = 0; i < chars.Length / 2; i++)
+            {
+                char temp = chars[i];
+                chars[i] = chars[j];
+                chars[j] = temp;
+                j--;
+            }
+            return new String(chars);
+        }
+    }
+}
