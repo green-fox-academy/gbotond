ff7cb2ab2b76454cef6c0613e4d9b75aca7780a9
GBotond <botond.gorfol@gmail.com>
2017-09-24 22:27:09 +0200

    DrawDiamond complete

---
+++ week-2/day-1/DrawDiamond/DrawDiamond/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace DrawDiamond
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Write a program that reads a number from the standard input, then draws a
+            // diamond like this:
+            //
+            //
+            //    *
+            //   ***
+            //  *****
+            // *******
+            //  *****
+            //   ***
+            //    *
+            //
+            // The diamond should have as many lines as the number was
+            int d = 7;
+            for (int i = 1; i <= d/2 + 1; i++)
+            {
+                for (int j = 0; j < d - i - 2; j++)
+                {
+                    Console.Write(" ");
+                }
+                for (int j = 0; j < i * 2 - 1; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+            for (int i = d/2; i >= 0; i--)
+            {
+                for (int j = 0; j < d - i - 2; j++)
+                {
+                    Console.Write(" ");
+                }
+                for (int j = 0; j < i * 2 - 1; j++)
+                {
+                    Console.Write("*");
+                }               
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+            Console.ReadLine();
+        }
+    }
+}
