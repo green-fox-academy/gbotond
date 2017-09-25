41efecee1a425dad6a4c83012fce211c0e3dea7e
GBotond <botond.gorfol@gmail.com>
2017-09-25 13:25:14 +0200

    PostIt not sure about it

---
+++ week-4/day-1/Post-It/Post-It/PostIt.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PostIt
+{
+    
+     public struct PostIt
+     {
+        public string Text;
+        public string BackgroundColor;
+        public string TextColor;
+     }
+    
+}
---
+++ week-4/day-1/Post-It/Post-It/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PostIt
+{
+    class Program
+    {              
+        static void Main(string[] args)
+        {
+            PostIt postIt1;
+            postIt1.Text = "Idea 1";
+            postIt1.BackgroundColor = "orange";
+            postIt1.TextColor = "blue";
+
+            PostIt postIt2;
+            postIt2.Text = "Awesome";
+            postIt2.BackgroundColor = "pink";
+            postIt2.TextColor = "black";
+
+            PostIt postIt3;
+            postIt3.Text = "Superb";
+            postIt3.BackgroundColor = "yellow";
+            postIt3.TextColor = "green";
+
+            Console.WriteLine(postIt1.Text + ", " + postIt1.BackgroundColor + ", " + postIt1.TextColor);
+            Console.WriteLine(postIt2.Text + ", " + postIt2.BackgroundColor + ", " + postIt2.TextColor);
+            Console.WriteLine(postIt3.Text + ", " + postIt3.BackgroundColor + ", " + postIt3.TextColor);
+            Console.ReadLine();
+        }
+    }
+}
