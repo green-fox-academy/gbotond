06864a0509d206eb59c3e184e4bb2ba39aff212f
GBotond <botond.gorfol@gmail.com>
2017-09-27 17:25:41 +0200

    CountLetters complete

---
+++ week-4/day-3/CountLetters/CountLetters/Counter.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CountLetters
+{
+    public class Counter
+    {
+        public Dictionary<char, int> StrToDictCount(string example)
+        {
+            Dictionary<char, int> returnDict = new Dictionary<char, int>();
+
+            if (String.IsNullOrEmpty(example))
+            {
+                return returnDict;
+            }
+
+            foreach (char letter in example)
+            {
+
+                if (!returnDict.ContainsKey(letter))
+                {
+                    returnDict.Add(letter, 1);
+                }
+
+                else
+                {
+                    returnDict[letter]++;
+                }
+            }
+
+            return returnDict;
+        }
+    }
+}
---
+++ week-4/day-3/CountLetters/CountLetters/Test.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CountLetters;
+using NUnit.Framework;
+
+namespace CountLetters
+{
+    [TestFixture]
+    public class Test
+    {
+        Counter counter = new Counter();
+
+        [Test]
+        public void TestDictionary()
+        {
+            string input = "information";
+
+            Dictionary<char, int> testDict = new Dictionary<char, int>()
+            {
+                { 'i', 2 },
+                { 'n', 2 },
+                { 'f', 1 },
+                { 'o', 2 },
+                { 'r', 1 },
+                { 'm', 1 },
+                { 'a', 1 },
+                { 't', 1 }
+            };
+
+            Assert.AreEqual(testDict, counter.StrToDictCount(input));
+        }
+    }
+}
