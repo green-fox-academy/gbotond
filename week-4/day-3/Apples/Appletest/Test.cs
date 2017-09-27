3c4fc71ada3c62f937b95a0e12ff6fc66c8258a5
GBotond <botond.gorfol@gmail.com>
2017-09-27 14:01:34 +0200

    Apples complete

---
+++ week-4/day-3/Apples/Appletest/Test.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Apples;
+using NUnit.Framework;
+
+namespace Appletest
+{
+    [TestFixture]
+    public class Test   
+    {
+        Apple apple = new Apple();
+
+        [Test]
+        public void TestGetApple()
+        {
+            string message = "apple";
+
+            Assert.AreEqual(message, apple.GetApple());
+        }
+    }
+}
