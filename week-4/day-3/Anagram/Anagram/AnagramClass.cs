46db5b82d214b087e8d95c5275786b90f8fb4e4f
GBotond <botond.gorfol@gmail.com>
2017-09-27 16:20:49 +0200

    Anagram complete

---
+++ week-4/day-3/Anagram/Anagram/AnagramClass.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Anagram
+{
+    public class AnagramClass
+    {
+        public static bool AnagramOrNot(string s1, string s2)
+        {
+            if (s1 != null && s2 != null)
+            {
+                char[] s1Chars = s1.ToCharArray();
+                Array.Sort(s1Chars);
+                char[] s2Chars = s2.ToCharArray();
+                Array.Sort(s2Chars);
+
+                bool anagram = true;
+
+                if (s1Chars.Length != s2Chars.Length)
+                {
+                    anagram = false;
+                }
+                else
+                {
+                    for (int i = 0; i < s1Chars.Length; i++)
+                    {
+                        if (s1Chars[i] != s2Chars[i])
+                        {
+                            anagram = false;
+                        }
+                    }
+                }
+                return anagram;
+            }
+            return false;
+        }
+    }
+}
---
+++ week-4/day-3/Anagram/Anagram/Test.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Anagram;
+using NUnit.Framework;
+
+namespace Anagram
+{
+    [TestFixture]
+    public class Test
+    {
+        AnagramClass anagram = new AnagramClass();
+
+        [Test]
+        public void TestAnagrams()
+        {
+            string first = "team";
+            string second = "meat";
+
+            bool output = true;
+
+            Assert.AreEqual(output, AnagramClass.AnagramOrNot(first, second));
+        }
+
+        [Test]
+        public void NotAnagram()
+        {
+            string first = "team";
+            string second = "greenfox";
+
+            bool output = false;
+
+            Assert.AreEqual(output, AnagramClass.AnagramOrNot(first, second));
+        }
+
+        [Test]
+        public void TestLonger()
+        {
+            string first = "team";
+            string second = "meeat";
+
+            bool output = false;
+
+            Assert.AreEqual(output, AnagramClass.AnagramOrNot(first, second));
+        }
+
+        [Test]
+        public void TestUpperCase()
+        {
+            string first = "team";
+            string second = "mEaT";
+
+            bool output = false;
+
+            Assert.AreEqual(output, AnagramClass.AnagramOrNot(first, second));
+        }
+    }
+}
