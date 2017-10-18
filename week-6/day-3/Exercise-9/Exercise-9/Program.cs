aa62065e6203f47d469f8d2d9e66241ed7e96364
GBotond <botond.gorfol@gmail.com>
2017-10-18 16:22:07 +0200

    Exercise-9 completed

---
+++ week-6/day-3/Exercise-9/Exercise-9/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] array = { 'a', 'l', 'p', 'a', 'g', 'a' };
+
+            string stringFromArrayLambda = String.Concat(array.Select(character => character));
+            Console.WriteLine(stringFromArrayLambda);
+
+            string stringFromArrayQuery = String.Concat(
+                                          from character in array
+                                          select character);
+            Console.WriteLine(stringFromArrayQuery);
+
+            Console.ReadLine();
+        }
+    }
+}
