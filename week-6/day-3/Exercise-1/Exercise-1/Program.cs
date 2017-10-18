486cd099bcc0eee5e445263cf3c3cb81bc5c614d
GBotond <botond.gorfol@gmail.com>
2017-10-18 11:53:05 +0200

    Exercise-1 completed

---
+++ week-6/day-3/Exercise-1/Exercise-1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 1, 3, -2, -4, -7, -3, -8, 12, 19, 6, 9, 10, 14 };
+
+            IEnumerable<int> evenNumbers1 =
+                from num in numbers
+                where num % 2 == 0
+                select num;
+
+            foreach (int i in evenNumbers1)
+            {
+                Console.Write(i + " ");
+            }
+            Console.WriteLine(System.Environment.NewLine);
+
+            IEnumerable<int> evenNumbers2 = numbers.Where(num => num % 2 == 0);
+
+            foreach (int  num in evenNumbers2)
+            {
+                Console.Write( + num + " ");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
