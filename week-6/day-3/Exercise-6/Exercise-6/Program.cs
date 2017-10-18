ad344d8ecf3d00681b835ca9f26043c1e54d57ef
GBotond <botond.gorfol@gmail.com>
2017-10-18 14:53:45 +0200

    Exercise-6 completed

---
+++ week-6/day-3/Exercise-6/Exercise-6/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = Console.ReadLine();
+
+            var lettersAndCountsLambda = str.GroupBy(x => x);
+
+            foreach (var item in lettersAndCountsLambda)
+            {
+                Console.WriteLine("{0} : {1}", item.Key, item.Count());
+            }
+
+            Console.WriteLine(System.Environment.NewLine);
+
+            var lettersAndCounsQuery = from s in str
+                                       group s by s
+                                       into x
+                                       select new { Letter = x.Key, Count = x.Count() };
+
+            foreach (var item in lettersAndCounsQuery)
+            {
+                Console.WriteLine("{0} : {1}", item.Letter, item.Count);
+            }
+            Console.ReadLine();
+        }
+    }
+}
