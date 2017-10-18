8f6f9fa83eac63210d86032b2c4048945ad38ea7
GBotond <botond.gorfol@gmail.com>
2017-10-18 13:05:01 +0200

    Exercise-2 completed

---
+++ week-6/day-3/Exercise-2/Exercise-2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 1, 3, -2, -4, -7, -3, -8, 12, 19, 6, 9, 10, 14 };
+
+            var oddAverageQuery = (from num in numbers
+                             where num % 2 != 0
+                             select num).Average();
+
+            Console.WriteLine(oddAverageQuery);
+
+            var oddAverageLambda = numbers.Where(num => num % 2 != 0).Average();
+
+            Console.WriteLine(oddAverageLambda);
+
+            Console.ReadLine();
+        }
+    }
+}
