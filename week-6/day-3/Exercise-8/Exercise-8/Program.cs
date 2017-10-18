797976e0ff7b47a2b38b14838d495fb4f1d96f26
GBotond <botond.gorfol@gmail.com>
2017-10-18 15:43:34 +0200

    Exercise-8 completed

---
+++ week-6/day-3/Exercise-8/Exercise-8/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = Console.ReadLine();
+
+            var upperCaseQuery = from s in str
+                                 where Char.IsUpper(s)
+                                 select s;
+            foreach (var item in upperCaseQuery)
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine(System.Environment.NewLine);
+
+            var upperCaseLambda = str.Where(s => Char.IsUpper(s));
+            foreach (var item in upperCaseLambda)
+            {
+                Console.Write(item + " ");
+            }
+            Console.ReadLine();
+        }
+    }
+}
