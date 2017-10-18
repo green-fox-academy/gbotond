7dcad7d0b56f386e959989b074ecb41ea965ece9
GBotond <botond.gorfol@gmail.com>
2017-10-18 13:39:22 +0200

    Exercise-4 completed

---
+++ week-6/day-3/Exercise-4/Exercise-4/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] number = new[] { 3, 9, 2, 8, 6, 5 };
+
+            var numberQuery = from num in number
+                              where num * num > 20
+                              select num;
+            foreach (int num in numberQuery)
+            {
+                Console.Write(num + " ");
+            }
+            Console.WriteLine(System.Environment.NewLine);
+
+            var numberLambda = number.Where(num => num * num > 20);
+            foreach (int num in numberLambda)
+            {
+                Console.Write(num + " ");
+            }
+            Console.ReadLine();
+        }
+    }
+}
