534d55f72e3dec9d082ae1779135285bc96cf717
GBotond <botond.gorfol@gmail.com>
2017-10-18 15:05:15 +0200

    Exercise-7 completed

---
+++ week-6/day-3/Exercise-7/Exercise-7/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] cities = { "ROME", "LONDON", "NAIROBI", "CALIFORNIA", "ZURICH",
+                                "NEW DELHI", "AMSTERDAM", "ABU DHABI", "PARIS" };
+            var solutionWithQuery = from c in cities
+                                    where c.StartsWith("A") && c.EndsWith("I")
+                                    select c;
+            foreach (var item in solutionWithQuery)
+            {
+                Console.WriteLine(item);
+            }
+
+            var solutionWithLambda = cities.Where(c => c.StartsWith("A") && c.EndsWith("I"));
+            foreach (var item in solutionWithQuery)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
