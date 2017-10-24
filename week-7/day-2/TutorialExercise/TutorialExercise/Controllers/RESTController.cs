9524e16d9fe117fb15f4453ed09bb5091c8b0ea0
GBotond <botond.gorfol@gmail.com>
2017-10-24 13:50:53 +0200

    Hello world rest app

---
+++ week-7/day-2/TutorialExercise/TutorialExercise/Controllers/RESTController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using TutorialExercise.Models;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace TutorialExercise.Controllers
+{
+    [Route("api")]
+    public class RESTController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("greeting")]
+        public IActionResult Greeting()
+        {
+            Greeting greeting = new Greeting()
+            {
+                Id = 1,
+                content = "Hello .netWorld!"
+            };
+
+            return new JsonResult(greeting);
+        }
+    }
+}
---
+++ week-7/day-2/TutorialExercise/TutorialExercise/Models/Greeting.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TutorialExercise.Models
+{
+    public class Greeting
+    {
+        public long Id { get; internal set; }
+        public string content;
+    }
+}
