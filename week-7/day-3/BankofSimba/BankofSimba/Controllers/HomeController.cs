89fb8df261ccfb067b235825b62ae9376905d02f
GBotond <botond.gorfol@gmail.com>
2017-10-25 11:57:41 +0200

    Classes and folders created

---
+++ week-7/day-3/BankofSimba/BankofSimba/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using BankofSimba.Models;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace BankofSimba.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: /<controller>/
+        [Route("Simba")]
+        public IActionResult Index()
+        {
+            BankAccount bankAccount = new BankAccount()
+            {
+                Name = "Simba",
+                Balance = 2000,
+                AnimalType = Animal.Lion
+
+            };
+            return View(bankAccount);
+        }
+    }
+}
