62023b8972621477948ff9eedddd54b4538fd9c6
GBotond <botond.gorfol@gmail.com>
2017-11-02 16:00:56 +0100

    Basic structure created

---
+++ week-7/day-5/NBAapp/NBAapp/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using NBAapp.Models;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace NBAapp.Controllers
+{
+    public class HomeController : Controller
+    {
+        Team team;
+    
+        public HomeController(Team team)
+        {
+            this.team = team;
+        }
+
+        [HttpGet]
+        [Route("/")]
+        public IActionResult Index()
+        {
+            var team = new Team()
+            {
+                Name = "Green Foxes",
+                Fundation = 2015
+            };
+
+            return View(team);
+        }
+    }
+}
---
+++ week-7/day-5/NBAapp/NBAapp/Models/Player.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NBAapp.Models
+{
+    public class Player
+    {
+        public string Name { get; set; }
+        public string Position { get; set; }
+        public int Age { get; set; }
+        public decimal Height { get; set; }
+        public double Weight { get; set; }
+    }
+}
