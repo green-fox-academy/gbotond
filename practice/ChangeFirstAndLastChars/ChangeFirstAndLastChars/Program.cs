225ba66f940f8dc9a68cc50218c326186ffff392
GBotond <botond.gorfol@gmail.com>
2017-11-03 17:21:34 +0100

    basic exercise

---
+++ practice/ChangeFirstAndLastChars/ChangeFirstAndLastChars/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChangeFirstAndLastChars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(FirstLast("practice"));
+
+            Console.ReadLine();
+        }
+
+        public static string FirstLast(string original)
+        {  
+            int len = original.Length;
+            return original[len - 1] + original.Substring(1, len - 2) + original[0];
+        }
+    }
+}
