421ba3bcdc983810ef5665f1554b2ef3957a936c
GBotond <botond.gorfol@gmail.com>
2017-11-03 17:15:19 +0100

    simple practice exercise

---
+++ practice/AddFirstCharToTheFrontAndBack/AddFirstCharToTheFrontAndBack/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddFirstCharToTheFrontAndBack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str;
+            Console.WriteLine("Pls enter a string");
+            str = Console.ReadLine();
+            if (str.Length >= 1)
+            {
+                var s = str.Substring(0, 1);
+                Console.WriteLine(s + str + s);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
