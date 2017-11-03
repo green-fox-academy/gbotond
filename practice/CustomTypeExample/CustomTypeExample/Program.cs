f0ea6f5dc22a504e3f64f38c8ef74ff76425cd2f
GBotond <botond.gorfol@gmail.com>
2017-11-03 17:29:40 +0100

    basic exercise

---
+++ practice/CustomTypeExample/CustomTypeExample/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace CustomTypeExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UnitConverter feetToInchesConverter = new UnitConverter(12);
+            UnitConverter milesToFeetConverter = new UnitConverter(5280);
+
+            Console.WriteLine(feetToInchesConverter.Convert(100));
+            Console.WriteLine(milesToFeetConverter.Convert(100));
+            Console.WriteLine(feetToInchesConverter.Convert(milesToFeetConverter.Convert(1)));
+
+            Console.ReadLine();
+        }
+    }
+}
