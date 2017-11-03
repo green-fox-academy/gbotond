777bea6e1e00726c75b093da653649040b25a949
GBotond <botond.gorfol@gmail.com>
2017-11-03 17:28:13 +0100

    basic exercise

---
+++ practice/CountSpecifiedCharInString/CountSpecifiedCharInString/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CountSpecifiedCharInString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "Practice, Practice, Practice!";
+            char[] chars = str.ToCharArray();
+            int count = 0;
+            foreach (char c in chars)
+            {
+                if (c == 't')
+                {
+                    count++;
+                }
+            }
+            Console.WriteLine(count);
+            Console.ReadLine();
+        }
+    }
+}
