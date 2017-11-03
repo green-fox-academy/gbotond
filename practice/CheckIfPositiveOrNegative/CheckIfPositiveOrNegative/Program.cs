3b6706e3d1a53ffd97ffd33c3b70456173cbe181
GBotond <botond.gorfol@gmail.com>
2017-11-03 17:26:08 +0100

    basic exercise

---
+++ practice/CheckIfPositiveOrNegative/CheckIfPositiveOrNegative/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CheckIfPositiveOrNegative
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter two positive or negatve integers");
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+
+            if (a > 0 && b > 0)
+            {
+                Console.WriteLine("Both integers are positive");
+            }
+            else if (a < 0 && b < 0)
+            {
+                Console.WriteLine("Both integers are negative");
+            }
+            else
+            {
+                Console.WriteLine("One is negative and one is positive");
+            }
+            Console.ReadLine();
+        }
+    }
+}
