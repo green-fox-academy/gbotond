8d17f0e568af41c529d2b9cc355c70c6a484733b
GBotond <botond.gorfol@gmail.com>
2017-11-03 17:25:06 +0100

    basic exercise

---
+++ practice/CheckIfInputNumberIsAMultiplyOf3or5/CheckIfInputNumberIsAMultiplyOf3or5/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CheckIfInputNumberIsAMultiplyOf3or5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int input = 33;
+
+            if (input > 0)
+            {
+                Console.WriteLine(input % 3 == 0 || input % 5 == 0);
+            }
+            Console.ReadLine();
+        }
+    }
+}
