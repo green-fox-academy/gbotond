8784e8b53b5082a77da96aae734583056975bd9e
GBotond <botond.gorfol@gmail.com>
2017-11-10 18:16:37 +0100

    basic exercise

---
+++ practice/ForLoopAddNumbers/ForLoopAddNumbers/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForLoopAddNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0;
+            for (int i = 0; i < 10; i++)
+            {
+                sum += i;
+            }
+            Console.WriteLine(sum);
+            Console.ReadLine();
+        }
+    }
+}
