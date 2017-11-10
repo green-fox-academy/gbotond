f5c115a3c30c8c2b6dd8d7abde7d493a9089a561
GBotond <botond.gorfol@gmail.com>
2017-11-10 18:12:40 +0100

    drawpyramid practice

---
+++ practice/DrawPyramidPractice/DrawPyramidPractice/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DrawPyramidPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 6;
+            for (int i = 1; i <= number; i++)
+            {
+                for (int j = i; j <= number - 1; j++)
+                {
+                    Console.Write(" ");
+                }
+                for (int k = 1; k <= 2 * i - 1; k++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+        }
+    }
+}
