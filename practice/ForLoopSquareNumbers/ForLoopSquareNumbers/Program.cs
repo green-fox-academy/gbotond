7f5c675f5c9369c8700ce45fa9a0c80413fd2c41
GBotond <botond.gorfol@gmail.com>
2017-11-10 18:17:10 +0100

    exercise

---
+++ practice/ForLoopSquareNumbers/ForLoopSquareNumbers/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForLoopSquareNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int square = 0;
+            for (int i = 0; i < 10; i++)
+            {
+                square = i * i;
+                Console.WriteLine(square);
+            }
+            Console.ReadLine();
+        }
+    }
+}
