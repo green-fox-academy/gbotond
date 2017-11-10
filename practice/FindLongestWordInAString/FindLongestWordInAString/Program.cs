1fcc0b2bc2a291a41e0482f4a3a91780f7d6bf4a
GBotond <botond.gorfol@gmail.com>
2017-11-10 18:16:02 +0100

    practice exercise

---
+++ practice/FindLongestWordInAString/FindLongestWordInAString/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindLongestWordInAString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string practice = "GreenFoxAcademy is your one in a lifetime chance. Do not screw it!.";
+
+            string[] words = practice.Split(' ');
+            string word = "";
+            int ctr = 0;
+            foreach (String s in words)
+            {
+                if (s.Length > ctr)
+                {
+                    word = s;
+                    ctr = s.Length;
+                }
+            }
+            Console.WriteLine(word);
+            Console.ReadLine();
+        }
+    }
+}
