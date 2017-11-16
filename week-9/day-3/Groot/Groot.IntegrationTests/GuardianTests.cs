4f8ad88eac113ef3dca6b4a1600141b5caeb2cfb
GBotond <botond.gorfol@gmail.com>
2017-11-16 10:25:20 +0100

    4 tests created for groot

---
+++ week-9/day-3/Groot/Groot.IntegrationTests/GuardianTests.cs
@@ -0,0 +1,59 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.TestHost;
+using System;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Groot.IntegrationTests
+{
+    public class GuardianTests
+    {
+        private HttpClient Client;
+        private TestServer Server;
+
+        public GuardianTests()
+        {
+            Server = new TestServer(new WebHostBuilder().UseStartup<Startup>());
+            Client = Server.CreateClient();
+        }
+
+        [Fact]
+        public async Task IndexShouldReturnOkStatus()
+        {
+            var response = await Client.GetAsync("/groot");
+
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+        }
+
+        [Fact]
+        public async Task ExpectedResponseIAmGroot()
+        {
+            string test = await Client.GetStringAsync("/groot?message=test");
+
+            Assert.Equal("{\"received\":\"test\",\"translated\":\"I am Groot!\"}", test);
+        }
+
+        [Fact]
+        public async Task ReturnExpectedResponse()
+        {
+            var response = await Client.GetAsync("/groot/message");
+
+            var responseJson = await response.Content.ReadAsStringAsync();
+
+            Assert.Equal("{\"received\":\"message\",\"translated\":\"I am Groot!\"}", responseJson);
+        }
+
+        [Fact]
+        public async Task ReturnExpectedResponseWithoutMessage()
+        {
+            var response = await Client.GetAsync("/groot");
+
+            var responseJson = await response.Content.ReadAsStringAsync();
+
+            Assert.Equal("{\"error\":\"I am Groot!\"}", responseJson);
+        }
+
+    }
+}
---
+++ week-9/day-3/Groot/Groot/Controllers/GuardianController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Groot.Controllers
+{
+    public class GuardianController : Controller
+    {
+        [HttpGet]
+        [Route("/groot")]
+        [Route("/groot/{message}")]
+        public IActionResult Index(string message)
+        {
+            if (message == null)
+            {
+                return Json(new { error = "I am Groot!" });
+            }
+            else
+            {
+                return Json(new { received = message, translated = "I am Groot!" });
+            }
+        }
+    }
+}
