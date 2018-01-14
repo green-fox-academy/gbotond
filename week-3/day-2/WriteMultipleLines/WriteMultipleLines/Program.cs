7b2721b0e6a402dd0420ef6e8d01069e51c347cc
Görföl Botond <botond.gorfol@gmail.com>
2018-01-14 12:04:40 +0100

    fix: writeline function parameters

---
+++ week-3/day-2/WriteMultipleLines/WriteMultipleLines/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace WriteMultipleLines
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\Users\Botond\Desktop\wml.txt";
+            string word = "greenfox";
+            int number = 5;
+            Writelines(path, word, number);
+        }
+        private static void Writelines(string path, string word, int number)
+        {
+            try
+            {
+                using (StreamWriter writer = new StreamWriter(path))
+                {
+                    for (int i = 0; i < number; i++)
+                    {
+                        writer.WriteLine(word);
+                    }                  
+                }
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Unable to write file: wml.txt");
+            }
+        }
+    }
+}
