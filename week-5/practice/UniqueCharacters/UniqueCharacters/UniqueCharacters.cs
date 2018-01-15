77eeee7bf206b50fc90a8d553aa089ab0be89bed
Görföl Botond <botond.gorfol@gmail.com>
2018-01-15 08:29:06 +0100

    upload test practice

---
+++ week-5/practice/UniqueCharacters/UniqueCharacters/UniqueCharacters.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UniqueCharacters
+{
+    public class UniqueCharacters
+    {
+        public static void Main(string[] args)
+        {
+            // Create a method called `UniqueCharacters` that takes a string as parameter
+            // and returns a list with the unique letters of the given string
+            // Create basic unit tests for it with at least 3 different test cases
+            // Print the characters from that list in the following format:
+            // "n", "g", "r", "m"
+
+            string input = Console.ReadLine();
+            foreach (var chars in GetUniqueCharacters(input))
+            {
+                Console.Write("\"{0}\", ", chars);
+            }
+            Console.ReadLine();
+        }
+
+        public static List<char> GetUniqueCharacters(string input)
+        {
+            input = input.ToLower();
+            char[] CharArray = input.ToCharArray();
+            var UniqChars = new List<char>();
+            for (int i = 0; i < CharArray.Length; i++)
+            {
+                if (!UniqChars.Contains(CharArray[i]))
+                {
+                    UniqChars.Add(CharArray[i]);
+                }
+            }
+            return UniqChars;
+        }
+    }
+}
---
+++ week-5/practice/UniqueCharacters/UniqueCharactersTest/TestClass.cs
@@ -0,0 +1,57 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using UniqueCharacters;
+
+namespace UniqueCharactersTest
+{
+    [TestFixture]
+    public class TestClass
+    {
+        [Test]
+        public void TestUniqueCharactersWhenSingleLetter()
+        {
+            string input = "a";
+            var output = new List<char> { 'a' };
+
+            Assert.AreEqual(output, UniqueCharacters.UniqueCharacters.GetUniqueCharacters(input));
+        }
+
+        [Test]
+        public void TestUniqueCharactersWhenSameLetterMultipleTimes()
+        {
+            string input = "bbbbb";
+            var output = new List<char> { 'b' };
+
+            Assert.AreEqual(output, UniqueCharacters.UniqueCharacters.GetUniqueCharacters(input));
+        }
+
+        [Test]
+        public void TestUniqueCharactersWhenUniqLettersSingleTime()
+        {
+            string input = "abc";
+            var output = new List<char> { 'a', 'b', 'c' };
+
+            Assert.AreEqual(output, UniqueCharacters.UniqueCharacters.GetUniqueCharacters(input));
+        }
+
+        [Test]
+        public void TestUniqueCharactersWhenUniqLettersMultipleTimes()
+        {
+            string input = "accabccbbaaacaabba";
+            var output = new List<char> { 'a', 'c', 'b' };
+
+            Assert.AreEqual(output, UniqueCharacters.UniqueCharacters.GetUniqueCharacters(input));
+        }
+
+
+        [Test]
+        public void TestUniqueCharactersWhenUniqLettersMultipleTimesUpperLowerCases()
+        {
+            string input = "AccabccbBaaaCaabba";
+            var output = new List<char> { 'a', 'c', 'b' };
+
+            Assert.AreEqual(output, UniqueCharacters.UniqueCharacters.GetUniqueCharacters(input));
+        }
+    }
+}
